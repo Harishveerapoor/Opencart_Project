4bb16292af0dbb9ebf1d09db44140671e17ce6bd
HAREESH VEERAPOOR <144092935+Harishveerapoor@users.noreply.github.com>
2025-08-22 13:16:46 +0530

    Update BaseClass.java

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -27,6 +28,7 @@ import com.beust.jcommander.Parameter;
 
 import org.apache.logging.log4j.Logger;  // this important one we need import for logger 
 import org.apache.logging.log4j.LogManager;// these also important
+
 
 
 
@@ -96,7 +98,12 @@ public Properties p;
 	    }
 	  
 	    
-	    
+	    	 ChromeOptions options = new ChromeOptions();
+		    options.addArguments("--headless");  // ✅ Run in headless mode
+		    options.addArguments("--no-sandbox");
+		    options.addArguments("--disable-dev-shm-usage");
+
+		    WebDriver driver = new ChromeDriver(options);
 	    
 	    
 	    
